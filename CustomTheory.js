063308922db152c5768e0efa98e7734b990ac394
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-04-24 15:20:20 +0800

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -284,7 +284,7 @@ var init = () => {
 
     //// Story chapters
 
-    chapter1 = theory.createStoryChapter(0, "e", "You started to find out\nthat as k approaches infinity\n(1 + 1/k)^k approaches e\nMaybe this will be useful in your research?", () => true);
+    chapter1 = theory.createStoryChapter(0, "e", String(n.cost) + "\nYou started to find out\nthat as k approaches infinity\n(1 + 1/k)^k approaches e\nMaybe this will be useful in your research?", () => true);
 
     updateAvailability();
 
@@ -306,7 +306,7 @@ var tick = (elapsedTime, multiplier) => {
 
     currency.value += currency.value.max(BigNumber.from(0)) != BigNumber.from(0) ? (Math.pow(-1, n.level) * (1 + Math.sin(n.level / 18 * Math.PI)) / (2 ** (1 + k2.level) - Math.PI ** (k3.level)) * (1 + 1 / (getK1(k1.level) + 1)) ** (getK1(k1.level) + 1) * dt * bonus * (x = (getC1(c1.level).pow(getC1Exponent(c1Exp.level)) *
 
-                                   getC2(c2.level).pow(getC2Exponent(c2Exp.level)) * getC3(c3.level) * BigNumber.from(Math.E).pow(c4.level) * BigNumber.from(Math.PI).pow(c5.level) * BigNumber.from(Math.E).pow(BigNumber.from(c6.level * Math.sqrt(2))))) ** 0.75) : 1.1 * n.cost * -currency.value;
+                                   getC2(c2.level).pow(getC2Exponent(c2Exp.level)) * getC3(c3.level) * BigNumber.from(Math.E).pow(c4.level) * BigNumber.from(Math.PI).pow(c5.level) * BigNumber.from(Math.E).pow(BigNumber.from(c6.level * Math.sqrt(2))))) ** 0.75) : 1.1 * -currency.value;
     
     let sum = 0
     
