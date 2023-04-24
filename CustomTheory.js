2bf02a9ad742288656f321b959013f79d30bc89f
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-04-24 15:54:08 +0800

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -1,5 +1,5 @@
 import { ExponentialCost, FreeCost, LinearCost } from "./api/Costs"
-import { Localization } from "./api/Localization
+import { Localization } from "./api/Localization"
 import { BigNumber } from "./api/BigNumber"
 import { theory } from "./api/Theory"
 import { Utils } from "./api/Utils"
