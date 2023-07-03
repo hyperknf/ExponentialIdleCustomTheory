6e6074c68fd19f09bd0b8810a04e29286424fe26
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-07-03 22:14:51 +0800

    a

--- CombinationsAndPermutations.js
+++ CombinationsAndPermutations.js
@@ -115,18 +115,32 @@ var init = () => {
     // NO ACHIEVEMENTS OR STORY CHAPTERS
 }
 
-function integerFactorial(number) {
-    number = BigNumber.from(number)
-    if (number <= 1) return 1
-    return number * integerFactorial(number - 1)
+var g = 7;
+var C = [0.99999999999980993, 676.5203681218851, -1259.1392167224028,771.32342877765313, -176.61502916214059, 12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
+var gamma = (z) => {
+    if (z < 0.5) return Math.PI / (Math.sin(Math.PI * z) * gamma(1 - z));
+    else {
+        z -= 1;
+
+        var x = BigNumber.from(C[0]);
+        for (var i = 1; i < g + 2; i++)
+        x += C[i] / (z + i);
+
+        var t = z + g + 0.5;
+        return Math.sqrt(2 * Math.PI) * t.pow(z + 0.5) * BigNumber.E.pow(-t) * x;
+    }
+}
+var factorial = (num) => {
+    if (num <= 2) return gamma(num + 1)
+    return BigNumber.from(BigNumber.from(2) * BigNumber.PI * num) * BigNumber.from(num / BigNumber.E).pow(num)
 }
 
 function combinations(n, r) {
-    return integerFactorial(n) / (integerFactorial(r) * integerFactorial(n - r))
+    return factorial(n) / (factorial(r) * factorial(n - r))
 }
 
 function permutations(n, r) {
-    return integerFactorial(n) / integerFactorial(n - r)
+    return factorial(n) / factorial(n - r)
 }
 
 function binomialTheorem(x, y, n) {
