c7a030edd33bb952fd9fbb46e4d0b201ad19adb1
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-07-29 10:03:15 +0800

    Update SomeFunctions.js

--- SomeFunctions.js
+++ SomeFunctions.js
@@ -25,7 +25,7 @@ var init = () => {
 
     // c1
     {
-        let getDesc = (level) => "c_1=" + getC1(level).toString(0);
+        let getDesc = (level) => "c_1=" + getC1(level);
         c1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(15, Math.log2(2))));
         c1.getDescription = (_) => Utils.getMath(getDesc(c1.level));
         c1.getInfo = (amount) => Utils.getMathTo(getDesc(c1.level), getDesc(c1.level + amount));
@@ -34,7 +34,7 @@ var init = () => {
     // c2
     {
         let getDesc = (level) => "c_2=" + getC2(level);
-        let getInfo = (level) => "c_2=" + getC2(level).toString(0);
+        let getInfo = (level) => "c_2=" + getC2(level);
         c2 = theory.createUpgrade(1, currency, new ExponentialCost(5, Math.log2(10)));
         c2.getDescription = (_) => Utils.getMath(getDesc(c2.level));
         c2.getInfo = (amount) => Utils.getMathTo(getInfo(c2.level), getInfo(c2.level + amount));
