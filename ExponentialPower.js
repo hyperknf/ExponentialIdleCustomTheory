b2c8c56b7270bb36b9d81f327755831941659585
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-07-31 19:34:17 +0800

    Update ExponentialPower.js

--- ExponentialPower.js
+++ ExponentialPower.js
@@ -1,9 +1,4 @@
 import { ExponentialCost, FreeCost, LinearCost } from "./api/Costs";
-import { Localization } from "./api/Localization";
-import { BigNumber } from "./api/BigNumber";
-import { theory } from "./api/Theory";
-import { ExponentialCost, FreeCost, LinearCost } from "./api/Costs";
-import { Localization } from "./api/Localization";
 import { BigNumber } from "./api/BigNumber";
 import { theory } from "./api/Theory";
 import { Utils } from "./api/Utils";
