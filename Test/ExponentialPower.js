a2a781a63141c92270da82977fb15efe34d3b4b6
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-08-08 15:45:51 +0800

    Update ExponentialPower.js

--- Test/ExponentialPower.js
+++ Test/ExponentialPower.js
@@ -234,7 +234,7 @@ var tick = (elapsedTime, multiplier) => {
     if (unlockE.level >= 2) E *= E2
     
     currency.value += dt * bonus * BigNumber.from(getK(k.level)) * (
-        unlock.level >= 1 && unlockE.level >= 1 ? (1 / E.pow(0.9)) : 1
+        unlock.level >= 1 && unlockE.level >= 1 ? E.pow(-0.9) : 1
     ) * (tertiary_display[0] = BigNumber.from(getC1(c1.level)).pow(getC2Balance(getC2(c2.level)) * (
         unlock.level >= 2 ? getX1(x1.level) : 1
     ))) * (
@@ -248,7 +248,7 @@ var tick = (elapsedTime, multiplier) => {
     theory.invalidateQuaternaryValues()
 
     updateMilestoneUpgradeInfo()
-    updateAvailability()
+    updateAvailability()Q
 }
 
 var formatQuaternaryEntry = (...args) => new QuaternaryEntry(...args)
