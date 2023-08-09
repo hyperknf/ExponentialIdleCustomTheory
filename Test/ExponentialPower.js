05a097ad64647e134b6d7a5a3e85e8c0cd1fa856
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-08-09 12:19:26 +0800

    Update ExponentialPower.js

--- Test/ExponentialPower.js
+++ Test/ExponentialPower.js
@@ -236,7 +236,7 @@ var tick = (elapsedTime, multiplier) => {
     if (unlockE.level >= 2) E *= E2
     
     const drho = dt * bonus * getK(k.level) * (
-        unlock.level >= 1 && unlockE.level >= 1 ? E.pow(-1) : 1
+        unlock.level >= 1 && unlockE.level >= 1 ? (1 / E.pow(0.9)) : 1
     ) * (tertiary_display[0] = getC1(c1.level).pow(getC2Balance(getC2(c2.level)) * (
         unlock.level >= 2 ? getX1(x1.level) : 1
     ))) * (
